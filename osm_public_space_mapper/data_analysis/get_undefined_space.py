684b960aee7c6c63ed5efe987d5e4abe6382d077
Ester <91192024+ester-t-s@users.noreply.github.com>
2023-04-20 10:01:24 +0200

    fix(get_undefined_space): adapt to new combined list of all defined space elements

--- osm_public_space_mapper/data_analysis/get_undefined_space.py
+++ osm_public_space_mapper/data_analysis/get_undefined_space.py
@@ -1,27 +1,23 @@
 from osm_public_space_mapper.utils.osm_element import OsmElement
+from osm_public_space_mapper.utils.geometry_element import GeometryElement
 from osm_public_space_mapper.utils.bounding_box import BoundingBox
 import shapely
 from shapely.geometry import LinearRing, Polygon, MultiPolygon, Point, MultiPoint, LineString, MultiLineString
 ShapelyGeometry = LinearRing | Polygon | MultiPolygon | Point | MultiPoint | LineString | MultiLineString
 
 
-def load(all_defined_space_lists: dict[str, list[OsmElement | ShapelyGeometry]], bbox: BoundingBox) -> MultiPolygon:
+def load(all_defined_space: list[OsmElement | GeometryElement], bbox: BoundingBox) -> GeometryElement:
     """returns space that is not part of all defined space as a Polygon
 
     Args:
-        all_defined_space_lists (dict[str,list[OsmElement | ShapelyGeometry]]): dictionary of all lists with defined space
+        all_defined_space (dict[str,list[OsmElement | GeometryElement]]): dictionary of all lists with defined space
         bbox (BoundingBox): BoundingBox in which the undefined space should be loaded
 
     Returns:
         MultiPolygon: undefined space within BoundingBox as Polyon
     """
-    defined_space_geometries = []
-    for list_name, elements in all_defined_space_lists.items():
-        for e in elements:
-            if type(e) == OsmElement:
-                defined_space_geometries.append(e.geom)
-            else:
-                defined_space_geometries.append(e)
-    defined_space_union = shapely.ops.unary_union(defined_space_geometries)
-    undefined_space = bbox.geom_projected.difference(defined_space_union)
+    defined_space_union = shapely.ops.unary_union([e.geom for e in all_defined_space])
+    undefined_space = GeometryElement(geometry=bbox.geom_projected.difference(defined_space_union),
+                                      access='yes',
+                                      space_category='undefined space')
     return undefined_space
