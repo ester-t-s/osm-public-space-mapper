513bf01ccd76f7484cc357f0d622ce73c5430759
Ester <91192024+ester-t-s@users.noreply.github.com>
2023-04-20 09:48:45 +0200

    fix(analyse_space_type): clean and remove duplicate steps

--- osm_public_space_mapper/data_analysis/analyse_space_type.py
+++ osm_public_space_mapper/data_analysis/analyse_space_type.py
@@ -26,22 +26,12 @@ def set_missing_space_types(elements: list[OsmElement]) -> None:
     """
     def set_space_type_for_construction(elements: list[OsmElement]) -> None:
         for e in elements:
-            if any([e.has_tag('construction'), e.has_tag('construction:highway'),
-                    e.tags.get('landuse') == 'construction', e.tags.get('highway') == 'construction', e.tags.get('railway') == 'construction']):
+            if any([e.has_tag('construction'),
+                    e.has_tag('construction:highway'),
+                    e.tags.get('landuse') == 'construction',
+                    e.tags.get('highway') == 'construction',
+                    e.tags.get('railway') == 'construction']):
                 e.space_type = 'construction'
-
-    def set_space_type_for_parking(elements: list[OsmElement]) -> None:
-        for e in [e for e in elements if e.is_polygon() or e.is_multipolygon()]:
-            if any([e.tags.get('amenity') in ['parking', 'parking_space'], e.has_tag('parking'), e.has_tag('motorcycle_parking')]):
-                e.space_type = 'parking'
-
-    def set_space_type_for_platforms(elements: list[OsmElement]) -> None:
-        tags_and_values_for_platforms = {'railway': 'platform', 'shelter_type': 'public_transport', 'public_transport': 'platform'}
-        for e in elements:
-            for tag in tags_and_values_for_platforms:
-                if e.tags.get(tag) == tags_and_values_for_platforms[tag]:
-                    e.space_type = 'public transport stop'
-                    break
 
     def set_space_type_from_tags(elements: list[OsmElement]) -> None:
         tags = ['leisure', 'amenity', 'natural', 'place', 'landuse']
@@ -49,8 +39,7 @@ def set_missing_space_types(elements: list[OsmElement]) -> None:
             for tag in tags:
                 if e.has_tag(tag):
                     e.space_type = e.tags.get(tag)
+                    break
 
     set_space_type_for_construction(elements)
-    set_space_type_for_parking(elements)
-    set_space_type_for_platforms(elements)
     set_space_type_from_tags(elements)
